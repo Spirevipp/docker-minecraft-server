1292d2f9e4740bc53d69e73bcba7933f36c93f17
Spirevipp <20364170+Spirevipp@users.noreply.github.com>
2025-09-17 01:30:43 +0200

    some logging and eula improvements

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "---Checking if specified RUNTIME_NAME exists---"
+echo "---Checking if specified RUNTIME_DIR exists---"
 if [ ! -f "${RUNTIME_DIR}/bin/java" ]; then
 	echo "---Runtime not found, add a valid path mapping such that RUNTIME_DIR points to a valid java version---"
 	sleep infinity
 else
 	echo "---Found the runtime---"
+	echo "RUNTIME_DIR = ${RUNTIME_DIR}"
 	$RUNTIME_DIR/bin/java --version
 fi
 
@@ -34,21 +35,7 @@ else
 	echo "---'server.properties' found..."
 fi
 chmod -R ${DATA_PERM} ${DATA_DIR}
-if [ ! -f $SERVER_DIR/eula.txt ]; then
-	:
-else
-	if [ "${ACCEPT_EULA}" == "false" ]; then
-		if grep -rq 'eula=true' ${SERVER_DIR}/eula.txt; then
-			sed -i '/eula=true/c\eula=false' ${SERVER_DIR}/eula.txt
-		fi
-		echo
-		echo "-------------------------------------------------------"
-		echo "------EULA not accepted, you must accept the EULA------"
-		echo "---to start the Server, putting server in sleep mode---"
-		echo "-------------------------------------------------------"
-		sleep infinity
-	fi
-fi
+
 echo "---Checking for old logs---"
 find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
 screen -wipe 2 &>/dev/null
@@ -56,14 +43,17 @@ screen -wipe 2 &>/dev/null
 if [ "${ACCEPT_EULA}" == "true" ]; then
 	if [ ! -f "$SERVER_DIR/eula.txt" ]; then
 		echo "---Accepting EULA---"
-		echo "eula=true" > "$SERVER_DIR/eula.txt"
+		echo "eula=true" >"$SERVER_DIR/eula.txt"
 	elif grep -rq 'eula=false' ${SERVER_DIR}/eula.txt; then
 		sed -i '/eula=false/c\eula=true' ${SERVER_DIR}/eula.txt
-		echo "---EULA accepted, server restarting, please wait...---"
-		sleep 5
-		exit 0
+		echo "---EULA accepted---"
 	fi
 elif [ "${ACCEPT_EULA}" == "false" ]; then
+	if [ ! -f "$SERVER_DIR/eula.txt" ]; then
+		echo "eula=false" >"$SERVER_DIR/eula.txt"
+	elif grep -rq 'eula=true' ${SERVER_DIR}/eula.txt; then
+		sed -i '/eula=true/c\eula=false' ${SERVER_DIR}/eula.txt
+	fi
 	echo
 	echo "-------------------------------------------------------"
 	echo "------EULA not accepted, you must accept the EULA------"
@@ -77,10 +67,20 @@ fi
 echo "---Starting Server---"
 cd ${SERVER_DIR}
 if [ -n "${CUSTOM_LAUNCH_SCRIPT}" ]; then
+	echo "---Starting server using custom launch script---"
+	echo "CUSTOM_LAUNCH_SCRIPT = ${CUSTOM_LAUNCH_SCRIPT}"
 	screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/${CUSTOM_LAUNCH_SCRIPT}
 elif [ -n "${JVM_CUSTOM_COMMAND}" ]; then
+	echo "---Starting server using custom JVM command---"
+	echo "JVM_CUSTOM_COMMAND = ${JVM_CUSTOM_COMMAND}"
 	screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${RUNTIME_DIR}/bin/java ${JVM_CUSTOM_COMMAND}
 else
+	echo "---Starting server using standard launch method---"
+	echo "EXTRA_JVM_PARAMS = ${EXTRA_JVM_PARAMS}"
+	echo "XMX_SIZE = ${XMX_SIZE}"
+	echo "XMS_SIZE = ${XMS_SIZE}"
+	echo "JAR_NAME = ${JAR_NAME}"
+	echo "GAME_PARAMS = ${GAME_PARAMS}"
 	screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${RUNTIME_DIR}/bin/java ${EXTRA_JVM_PARAMS} -Xmx${XMX_SIZE}M -Xms${XMS_SIZE}M -jar ${SERVER_DIR}/${JAR_NAME}.jar nogui ${GAME_PARAMS}
 fi
 sleep 2
